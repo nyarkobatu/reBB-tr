1f85d48947198ebd8b44de0b6dd68c3594e4e6fd
Biscuit <gtaw@booskit.dev>
2025-02-24 20:37:39 +0100

    updated github repo

--- site.php
+++ site.php
@@ -2,5 +2,5 @@
 define('SITE_NAME', 'reBB');
 define('SITE_DESCRIPTION', 'BBCode done differently');
 define('SITE_URL', 'https://rebb.booskit.dev');
-define('FOOTER_GITHUB', '#');
+define('FOOTER_GITHUB', 'https://github.com/booskit-codes/reBB');
 ?>
