82e4e73b602afcd14d1039c016e2a5a5ff589ff7
Biscuit <gtaw@booskit.dev>
2025-02-25 08:36:54 +0100

    1.1.1 - hotfix escape strings

--- builder.php
+++ builder.php
@@ -126,9 +126,11 @@ if (isset($_GET['f']) && !empty($_GET['f'])) {
             let componentCounter = 0;
             let builderInstance;
 
-            let existingFormData = <?php echo $existingSchema ? $existingSchema : 'null'; ?>;
-            let existingFormNamePHP = "<?php echo $existingFormName; ?>";
-            let existingTemplatePHP = "<?php echo $existingTemplate; ?>";
+            let existingFormData = <?php echo $existingSchema ? json_encode($existingSchema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) : 'null'; ?>;
+            let existingFormNamePHP = "<?php echo htmlspecialchars($existingFormName); ?>";
+            let existingTemplatePHP = <?php echo json_encode($existingTemplate, JSON_UNESCAPED_SLASHES); ?>;
+
+
 
 
             Formio.builder(builderElement, existingFormData, {
