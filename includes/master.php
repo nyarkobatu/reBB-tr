994270eef58d9bc41169c11999290e0018a7ee2d
Biscuit <gtaw@booskit.dev>
2025-03-05 12:41:45 +0100

    1.3.9 - site refactor admin.php

--- includes/master.php
+++ includes/master.php
@@ -96,7 +96,7 @@ function get_page_title() {
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script><?php yield_js_vars(); ?></script>
     <script src="<?php echo ASSETS_DIR; ?>/js/common.js?v=<?php echo SITE_VERSION; ?>"></script>
     <?php yield_javascript(); ?>
