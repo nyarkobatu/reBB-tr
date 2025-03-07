ce6ed0c783ea24d255c79418aa0a201be26d12af
Biscuit <gtaw@booskit.dev>
2025-03-07 14:07:52 +0100

    1.4.0a - hotfix

--- kernel.php
+++ kernel.php
@@ -9,7 +9,7 @@
 
 // Define the base path for the application
 if (!defined('APP_VERSION')) {
-    define('APP_VERSION', 'v1.4.0');
+    define('APP_VERSION', 'v1.4.0a');
 }
 
 // Define the public path for the application
