fc5ffde9aed1d7630473841e12bab82f045addff
Biscuit <gtaw@booskit.dev>
2025-03-10 21:50:52 +0100

    small fix for the meta tags

--- includes/master.php
+++ includes/master.php
@@ -84,7 +84,7 @@ function generate_meta_tags() {
     // Default values
     $title = get_page_title();
     $description = SITE_DESCRIPTION ?? 'BBCode done differently';
-    $image = site_url('resources/android-chrome-512x512.png');
+    $image = site_url('resources/android-chrome-192x192.png');
     $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     $type = 'website';
     
