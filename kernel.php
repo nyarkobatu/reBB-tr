361ba1e68e2451a01648462912796241f4bc3024
Biscuit <gtaw@booskit.dev>
2025-03-10 12:15:51 +0100

    1.4.3a - bump version

--- kernel.php
+++ kernel.php
@@ -9,7 +9,7 @@
 
 // Define the base path for the application
 if (!defined('APP_VERSION')) {
-    define('APP_VERSION', 'v1.4.3');
+    define('APP_VERSION', 'v1.4.3a');
 }
 
 // Define the public path for the application
